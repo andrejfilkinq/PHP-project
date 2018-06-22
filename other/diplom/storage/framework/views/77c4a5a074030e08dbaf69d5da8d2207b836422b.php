3bc20ff43302af576ef7d68d1b6f8c960dc2684c
kolinmorgan333 <kolin.morgan333@gmail.com>
2018-06-22 12:12:13 +0300

    release v5

--- other/diplom/storage/framework/views/77c4a5a074030e08dbaf69d5da8d2207b836422b.php
+++
@@ -1,7 +0,0 @@
-<h1>Hello mail</h1>
-
-<a href="<?php echo e(route('email.confirmation',['tokean'=>$user->token])); ?>">
-    <?php echo e(route('email.confirmation',['tokean'=>$user->token])); ?>
-
-</a>
-
